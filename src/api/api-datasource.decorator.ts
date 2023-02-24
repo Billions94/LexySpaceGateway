25fb54d3ac67627e6c3487bea6c2d0810cc01c69
Ejiroghene Alexander Egbedi <E.a.egbedi@gmail.com>
2023-02-24 13:44:50 +0100

    Minor update

--- src/api/api-datasource.decorator.ts
+++ src/api/api-datasource.decorator.ts
@@ -22,7 +22,9 @@ export class ApiDatasourceDecorator
   ) {
     super();
     super.initialize({ context, cache });
-    this.baseURL = process.env.API_BASE_URL;
+    this.baseURL = process.env.API_BASE_URL
+      ? process.env.API_BASE_URL
+      : 'https://lexyspace-api.onrender.com/api';
   }
 
   public get<TResult = any>(
