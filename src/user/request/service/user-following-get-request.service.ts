f5522a3de5fe1480096056313311d16102e42929
Ejiroghene Alexander Egbedi <E.a.egbedi@gmail.com>
2023-08-22 19:29:46 +0200

    Bug fix and code update directly from develop

--- src/user/request/service/user-following-get-request.service.ts
+++ src/user/request/service/user-following-get-request.service.ts
@@ -1,6 +1,6 @@
 import { AbstractRequestService } from '../../../core/request/abstract-request.service';
 import { api } from '../../../api/api';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { User } from '../../../dto';
 import { UserResponseMapper } from '../../response/user-response.mapper';
 
@@ -22,8 +22,6 @@ export class UserFollowersRequestService extends AbstractRequestService {
       requestHandler,
       parameterHandler
     );
-
-    Logger.debug(response);
 
     return this.userResponseMapper.mapUsers(response.followers);
   }
