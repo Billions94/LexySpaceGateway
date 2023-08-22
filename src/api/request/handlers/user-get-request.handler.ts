ba4bfce4d9e0517ddd9d681c7ff52f95001d7332
Ejiroghene Alexander Egbedi <E.a.egbedi@gmail.com>
2023-08-22 19:27:02 +0200

    Bug fix and code update directly from develop

--- src/api/request/handlers/user-get-request.handler.ts
+++ src/api/request/handlers/user-get-request.handler.ts
@@ -6,7 +6,7 @@ export class UserGetRequestHandler
   extends AbstractRequestHandler
   implements GetRequestHandlerInterface
 {
-  protected path = 'users';
+  protected path = 'users/me';
 
   async get(params?: URLSearchParams, init?: RequestInit) {
     init = this.forwardAuthHeader(init);
